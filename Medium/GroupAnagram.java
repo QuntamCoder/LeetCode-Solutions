79118b5253e3392b8a7d0dedcc15f96db1ab519e
Amol jadhav <124095673+QuntamCoder@users.noreply.github.com>
2026-04-09 12:20:11 +0530

    solved leetcode group angram problem

---
+++ Medium/GroupAnagram.java
@@ -0,0 +1,24 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagram{
+     public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String,List<String>> map=new HashMap<>();
+        for(String str:strs){
+            char[] ch=str.toCharArray();
+            Arrays.sort(ch);
+            String sorted=new String(ch);
+            if(!map.containsKey(sorted)){
+                map.put(sorted, new ArrayList<>());
+            }
+            map.get(sorted).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+
+    }
+}
